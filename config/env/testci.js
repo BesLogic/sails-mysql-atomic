af43372a56e5d35be0bb6c8af2a5e86ba48bcb78
Samuel Poirier <samuel.poirier@beslogic.com>
2016-12-16 13:05:23 -0500

    Added testci environment for travis

---
+++ config/env/testci.js
@@ -0,0 +1,42 @@
+const winston = require('winston');
+
+module.exports = {
+
+    connections: {
+        mySQLT: {
+            adapter: 'sails-mysql-transactions',
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'mysqltransactionshelper',
+
+            transactionConnectionLimit: 20,
+            rollbackTransactionOnError: false,
+            queryCaseSensitive: false,
+
+            replication: {
+                enabled: false
+            }
+
+        }
+    },
+
+    models: {
+        connection: 'mySQLT',
+        migrate: 'drop'
+    },
+
+    log: {
+        'level': 'debug',
+        'custom': new (winston.Logger)({
+            'transports': [
+                new (winston.transports.Console)({
+                    'level': 'verbose',
+                    'colorize': true,
+                    'timestamp': true,
+                    'json': false
+                })
+            ]
+        })
+    }
+};
